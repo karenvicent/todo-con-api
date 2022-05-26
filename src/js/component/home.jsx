021aa7e0a44f2b50961dd362a85f3b054ddd1abe
karenvicent <karenvergara@msn.com>
2022-05-26 01:37:07 +0000

    primer todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,60 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [tareas, setTareas] = useState("");
+	const [listaTareas, setListaTareas] = useState([]);
+	const agregarTareas = (e) => {
+		e.preventDefault();
+		let tempLista = [...listaTareas];
+		tempLista.push(tareas);
+		setListaTareas(tempLista);
+		setTareas("");
+	};
+
+	const handleInputChange = (e) => {
+		setTareas(e.target.value);
+	};
+
+	const eliminarTareas = (indice) => {
+		let listaRemovida = listaTareas.filter(
+			(item, posicion) => posicion !== indice
+		);
+		setListaTareas(listaRemovida);
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="caja">
+			<div className="titulo mx-auto" style={{ width: "50rem" }}>
+				<h1 className="text-center display-1">todos</h1>
+			</div>
+			<div className="card mx-auto" style={{ width: "50rem" }}>
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item d-flex">
+						<form
+							className="me-auto p-2 text-muted fs-4 border-0 container-fluid"
+							onSubmit={agregarTareas}>
+							<input
+								className="border-0 text-muted fs-4 container-fluid"
+								type="text"
+								placeholder="What needs to be done?"
+								onChange={handleInputChange}
+								value={tareas}></input>
+						</form>
+					</li>
+					{listaTareas.map((element, indice) => (
+						<li className="list-group-item d-flex" key={indice}>
+							<p className="me-auto p-2 text-muted fs-4">
+								{element}
+							</p>
+							<button
+								className="btn fs-4 btn-outline-light border-0"
+								onClick={() => eliminarTareas(indice)}>
+								X
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
